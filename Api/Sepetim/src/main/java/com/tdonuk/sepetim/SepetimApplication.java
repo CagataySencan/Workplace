06dc2ce3f092ced5e27c408169ac5a5986653867
taha <rtdonuk@gmail.com>
2023-01-16 23:18:24 +0300

    initial commit

---
+++ Api/Sepetim/src/main/java/com/tdonuk/sepetim/SepetimApplication.java
@@ -0,0 +1,13 @@
+package com.tdonuk.sepetim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepetimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepetimApplication.class, args);
+	}
+
+}
---
+++ Api/Sepetim/src/test/java/com/tdonuk/sepetim/SepetimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdonuk.sepetim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepetimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
