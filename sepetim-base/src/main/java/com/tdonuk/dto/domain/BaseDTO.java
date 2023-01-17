a8eeea90758c1e7b3d91dee4df50fd86974cad9b
taha <rtdonuk@gmail.com>
2023-01-18 00:44:50 +0300

    sepetim-base architecture update

--- sepetim-base/src/main/java/com/tdonuk/dto/domain/BaseDTO.java
+++ sepetim-base/src/main/java/com/tdonuk/dto/domain/BaseDTO.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 @Data
 public class BaseDTO {
-    private String id;
-    private Date created;
-    private Date lastUpdated;
+    protected String id;
+    protected Date created;
+    protected Date lastUpdated;
 }
