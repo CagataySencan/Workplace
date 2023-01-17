9cd4e13a2f752167b5ed5d342ef845bd36b6eb75
taha <rtdonuk@gmail.com>
2023-01-18 00:31:31 +0300

    sepetim-base architecture update

---
+++ sepetim-base/src/main/java/com/tdonuk/config/Environment.java
@@ -0,0 +1,5 @@
+package com.tdonuk.config;
+
+public class Environment {
+    public static String FIREBASE_API_KEY = "[PROTECTED]";
+}
--- sepetim-base/src/main/java/com/tdonuk/constant/App.java
+++ sepetim-base/src/main/java/com/tdonuk/constant/App.java
@@ -8,6 +8,4 @@ public final class App {
     public static final String APP_NAME = "Sepetim";
     public static final String URL = "sepetim.azurewebsites.net";
     public static final String VERSION = "1.0.0";
-    public static final String API_KEY = "[PROTECTED]";
-    public static final String FIREBASE_API_KEY = "[PROTECTED]";
 }
