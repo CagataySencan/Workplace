a90f6da93f2baf0cb85f338d795812b817857868
taha <rtdonuk@gmail.com>
2023-01-20 19:13:11 +0300

    sepetim-base update

--- sepetim-base/src/main/java/com/tdonuk/dto/domain/CurrentActualDTO.java
+++ sepetim-base/src/main/java/com/tdonuk/dto/domain/CurrentActualDTO.java
@@ -7,8 +7,13 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class CurrentActualDTO extends BaseDTO {
+public class CurrentActualDTO extends BaseDTO implements Comparable<CurrentActualDTO> {
     private Integer vendorCount;
     private Integer productCount;
     private List<VendorDTO> vendors;
+
+    @Override
+    public int compareTo(CurrentActualDTO o) {
+        return getCreated().compareTo(o.getCreated());
+    }
 }
