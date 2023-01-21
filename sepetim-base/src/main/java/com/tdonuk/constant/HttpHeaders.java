8ca96e82dbed06961b3defdd287e319b1048ce0e
taha <rtdonuk@gmail.com>
2023-01-21 16:20:21 +0300

    sepetim-base update

---
+++ sepetim-base/src/main/java/com/tdonuk/constant/HttpHeaders.java
@@ -0,0 +1,8 @@
+package com.tdonuk.constant;
+
+public class HttpHeaders {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String REFRESH_TOKEN = "refresh_token";
+}
