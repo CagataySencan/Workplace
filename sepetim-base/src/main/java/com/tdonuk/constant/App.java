4f8da9e2298475560164f3d23076c24903914c31
taha <rtdonuk@gmail.com>
2023-01-22 13:37:08 +0300

    url update

--- sepetim-base/src/main/java/com/tdonuk/constant/App.java
+++ sepetim-base/src/main/java/com/tdonuk/constant/App.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class App {
     public static final String APP_NAME = "Sepetim";
-    public static final String URL = "sepetim.azurewebsites.net";
+    public static final String URL = "https://sepetim.azurewebsites.net";
     public static final String VERSION = "1.0.0";
 }
