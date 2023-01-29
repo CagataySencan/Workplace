299bd8b3509f9b4146e7abfe7e20be02eadc3b51
taha <rtdonuk@gmail.com>
2023-01-29 21:03:55 +0300

    endpoints updated

--- sepetim-base/src/main/java/com/tdonuk/constant/Endpoints.java
+++ sepetim-base/src/main/java/com/tdonuk/constant/Endpoints.java
@@ -1,17 +1,18 @@
 package com.tdonuk.constant;
 
 public class Endpoints {
-    public static final String ROOT = App.URL;
-    public static final String VERSION = "v1";
-    public static final String API = "api";
+    public static final String ROOT = App.URL + "/api/v1";
 
-    public static final String AUTH = "auth";
-    public static final String AUTH_PATH = String.join("/", ROOT, VERSION, API, AUTH);
-    public static final String LOGIN = AUTH_PATH +"/login";
-    public static final String REGISTER = AUTH_PATH+"/register";
-    public static final String ME = AUTH_PATH+"/me";
+    public static final String AUTH = ROOT+"/auth";
+    public static final String LOGIN = AUTH + "/login";
+    public static final String REGISTER = AUTH + "/register";
 
-    public static final String DISCOUNTS = "discounts";
-    public static final String DISCOUNTS_PATH = String.join("/", ROOT, VERSION, API, DISCOUNTS);
-    public static final String DISCOUNT_HIST = DISCOUNTS_PATH+"/hist";
+    public static final String USERS = ROOT + "/users";
+    public static final String ME = ROOT+"/me";
+
+    public static final String DISCOUNTS = ROOT+"/discounts";
+    public static final String DISCOUNT_HIST = DISCOUNTS+"/hist";
+
+    public static final String VENDORS = ROOT + "/vendors";
+    public static final String SUPPORTED_VENDORS = VENDORS + "/supported";
 }
